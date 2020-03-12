edbd9e1e42a4dfa95f0b23a1ff14a86e56de0a4b
Zain Ali <45647638+zainalimatharu@users.noreply.github.com>
2020-03-12 16:42:42 +0500

    your recommendation design complete

--- src/components/yourRecommendation/extras/Extras.js
+++ src/components/yourRecommendation/extras/Extras.js
@@ -8,47 +8,7 @@ import {
 } from '../../../redux/actions/yourRecommendation/recommendationAction';
 
 class Extras extends Component {
-  componentDidUpdate(prevProps) {
-    if (this.props.extras !== prevProps.extras) {
-      const extras = document.getElementById(
-        'recommendation-secondpage-content'
-      );
-
-      let lengthSum = 0;
-      let wantedIndex = 0;
-      for (let i = 0; i < extras.childNodes.length; i++) {
-        if (lengthSum < extras.clientHeight) {
-          lengthSum =
-            lengthSum +
-            extras.childNodes[i].clientHeight +
-            (i === 1 || i === 2 || i === 3 ? 16 : 25);
-          wantedIndex = i;
-        }
-      }
-
-      // console.log(extras.clientHeight, lengthSum, wantedIndex);
-      const toRemoveNo = extras.childNodes.length - wantedIndex;
-
-      let removedChild;
-      let payload = [];
-      for (let i = 0; i < toRemoveNo; i++) {
-        removedChild = extras.removeChild(
-          extras.childNodes[extras.childNodes.length - 1]
-        );
-        payload.unshift(removedChild);
-      }
-
-      console.log(payload);
-      this.props.setExtrasFromPageTwo(payload);
-    }
-  }
-
-  componentWillUnmount() {
-    this.props.clearExtrasFromPageTwo();
-  }
-
   render() {
-    console.log('extra 1');
     const { extras } = this.props;
     return (
       <>
